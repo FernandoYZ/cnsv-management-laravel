5d5fc1484e34f4d2cd2cffcec6d6f7b573f0a101
FernandoYZ <fernandozavala266@gmail.com>
2025-03-13 11:28:10 -0500

    fix(migration): agregar valores por defecto y mejorar la foreign key

--- database/migrations/2025_03_10_195654_create_paginas_table.php
+++ database/migrations/2025_03_10_195654_create_paginas_table.php
@@ -15,9 +15,18 @@ return new class extends Migration
             $table->id();
             $table->string('slug', 255)->unique();
             $table->string('titulo', 255);
-            $table->string('meta_description', 500)->nullable();
-            $table->string('meta_keywords', 255)->nullable();
-            $table->foreignId('imagen_destacada_id')->nullable()->constrained('archivos_multimedia')->nullOnDelete();
+            $table->string('meta_description', 500)
+                ->default('El Colegio Nuestra Señora del Valle ofrece educación integral, formación en valores y seminario menor en modalidad de residencia. Descubre nuestra propuesta educativa única.')
+                ->nullable();
+            $table->string('meta_keywords', 255)
+                ->default('educación primaria, educación secundaria, seminario menor, educación religiosa, valores, formación integral, colegio con residencia, colegio internado')
+                ->nullable();
+            $table->foreignId('imagen_destacada_id')
+                ->nullable()
+                ->constrained('archivos_multimedia')
+                ->onDelete('set null')
+                ->index()
+                ->name('fk_paginas_imagen_destacada_id');
             $table->integer('orden')->default(0);
             $table->string('tipo', 50)->nullable()->index(); // enum TipoPagina
             $table->boolean('estado')->default(false);
